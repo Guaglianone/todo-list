e651682b509576156c38d812e5fd83425f03a096
Cristian <cristianguaglianone@gmail.com>
2023-02-05 00:55:48 +0100

    starter

---
+++ script.js
@@ -0,0 +1,10 @@
+// SELECTORS
+const body = document.querySelector("body");
+const form = document.querySelector("form");
+const todoInput = document.querySelector(".todo-input");
+const filterOption = document.querySelector(".filter-todo");
+const todoList = document.querySelector(".todo-list");
+
+// FUNCTIONS
+
+// EVENT LISTENERS
